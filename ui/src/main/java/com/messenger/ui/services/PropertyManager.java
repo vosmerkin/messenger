da058fc5bf84185d7922c9c62a13d2c76404f3ef
Evgen <vosmerkin@mail.ru>
2022-10-14 00:25:19 +0300

    propsPRfixes

--- ui/src/main/java/com/messenger/ui/services/PropertyManager.java
+++ ui/src/main/java/com/messenger/ui/services/PropertyManager.java
@@ -1,11 +1,15 @@
 package com.messenger.ui.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertyManager {
+    private static final Logger log = LoggerFactory.getLogger(PropertyManager.class);
     private final static String NETWORK_PROPERTY_FILE_NAME = "src/main/resources/network.properties";
 
     private static final Properties property = new Properties();
@@ -13,13 +17,12 @@ public class PropertyManager {
     static {
         try (InputStream fis = new FileInputStream(NETWORK_PROPERTY_FILE_NAME)) {
             property.load(fis);
-        }
-        catch (IOException e) {
-            System.err.println("ОШИБКА: Файл свойств отсуствует!");
+        } catch (IOException e) {
+            log.debug("Error: Property file not found!");
         }
     }
 
-    public static String GetProperty(String propertyName) {
+    public static String getProperty(String propertyName) {
         return property.getProperty("propertyName");
     }
 
