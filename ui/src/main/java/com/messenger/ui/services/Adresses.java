ab6b3088b514efb7d6f3803f359a4b9206a1f967
vosmerkin <33931167+vosmerkin@users.noreply.github.com>
2022-11-11 17:49:33 +0200

    Update Adresses.java

--- ui/src/main/java/com/messenger/ui/services/Adresses.java
+++ ui/src/main/java/com/messenger/ui/services/Adresses.java
@@ -4,7 +4,8 @@ public class Adresses {
 
     private static String host1 = "http://192.168.31.35:8080";
     private static String host2 = "http://localhost:8080";
-    private static String host = host2;
+    private static final String host = "http://3.86.107.142:8080";
+    //private static String host = host2;
     public static final String CREATE = host + "/CRUDaddnames";
     public static final String UPDATE = host + "/CRUDupdatenames";
     public static final String DELETE = host + "/CRUDdeletenames?name=";
