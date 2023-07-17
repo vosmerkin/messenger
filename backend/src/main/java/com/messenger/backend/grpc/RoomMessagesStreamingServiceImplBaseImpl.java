9d1ecb9d00778cfa7f0eae651f2ac734b57272a7
Evgeny Melnik <MelnikEV@ksimex.com.ua>
2023-07-17 15:09:23 +0300

    protoc_generated_classes

--- backend/src/main/java/com/messenger/backend/grpc/RoomMessagesStreamingServiceImplBaseImpl.java
+++ backend/src/main/java/com/messenger/backend/grpc/RoomMessagesStreamingServiceImplBaseImpl.java
@@ -1,8 +1,13 @@
 package com.messenger.backend.grpc;
 
+import grpc_generated.RoomMessagesResponse;
+import grpc_generated.RoomMessagesStreamingServiceGrpc;
+import grpc_generated.RoomRequest;
 import io.grpc.stub.StreamObserver;
 
-public class RoomMessagesStreamingServiceImplBaseImpl  {
-
-
+public class RoomMessagesStreamingServiceImplBaseImpl extends RoomMessagesStreamingServiceGrpc.RoomMessagesStreamingServiceImplBase {
+    @Override
+    public void messageStreaming(RoomRequest request, StreamObserver<RoomMessagesResponse> responseObserver) {
+        super.messageStreaming(request, responseObserver);
+    }
 }
